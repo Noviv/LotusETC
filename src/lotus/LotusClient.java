ba13e91afd0e1a09a5be67327fa51f9da4960a43
Noviv <matthewwebb65@yahoo.com>
2016-10-01 11:15:32 -0500

    working on book command

--- src/lotus/LotusClient.java
+++ src/lotus/LotusClient.java
@@ -7,17 +7,56 @@ import java.net.Socket;
 
 public class LotusClient {
 
-    public static void main(String[] args) {
-        try {
-            Socket skt = new Socket("test-exch-lotus", 20000);
-            BufferedReader from_exchange = new BufferedReader(new InputStreamReader(skt.getInputStream()));
-            PrintWriter to_exchange = new PrintWriter(skt.getOutputStream(), true);
+    private Socket exch;
+    private BufferedReader from_exch;
+    private PrintWriter to_exch;
 
-            to_exchange.println("HELLO LOTUS");
-            String reply = from_exchange.readLine().trim();
-            System.err.printf("The exchange replied: %s\n", reply);
+    public LotusClient() {
+        try {
+            exch = new Socket("test-exch-lotus", 20000);
+            from_exch = new BufferedReader(new InputStreamReader(exch.getInputStream()));
+            to_exch = new PrintWriter(exch.getOutputStream(), true);
         } catch (Exception e) {
-            e.printStackTrace(System.out);
+            System.err.println("Error in LotusClient init: " + e.getMessage());
+            System.exit(0);
         }
+
+        try {
+            to_exch.println("HELLO LOTUS");
+            String reply = from_exch.readLine().trim();
+            System.out.println("Exchange init:");
+            System.out.println("\t" + reply);
+        } catch (Exception e) {
+            System.err.println("Error in LotusClient handshake: " + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    public void command(String cmd) {
+        try {
+            to_exch.println(cmd);
+            String reply = from_exch.readLine().trim();
+            System.out.println("Sent:" + cmd);
+            System.out.println("Exchange reply: " + reply);
+            System.out.println();
+        } catch (Exception e) {
+            System.out.println("Error in test: " + e.getMessage());
+        }
+    }
+
+    public void test() {
+        int i = 0;
+        while (i++ < 10) {
+            try {
+                System.out.println(from_exch.readLine().trim());
+                Thread.sleep(500);
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        LotusClient client = new LotusClient();
+        client.test();
     }
 }
