9c0d074c6e4d14d692e1a6ad8033bd7b719bde5c
copperstick6 <han818william@hotmail.com>
2016-10-01 12:19:43 -0500

    checks if it's worth it to convert to VALBZ/VALE

---
+++ src/checkingValbz.java
@@ -0,0 +1,17 @@
+public class checkingVALBZ
+{
+    public final int VALBZfee = 10;
+    public boolean worthItToConvert(Stocks(int,int,int,int,int,int,int)a)
+    {
+      int totVal = 0;
+      if(a.getVALBZamt()>=0||a.getVALE>=0)
+      {
+        totVal = 1*a.getVALBZ.getBuyVal()+VALBZfee;
+        if(totVal<a.getVALE.getBuyVal())
+        {
+          return true;
+        }
+      }
+      return false;
+    }
+}
